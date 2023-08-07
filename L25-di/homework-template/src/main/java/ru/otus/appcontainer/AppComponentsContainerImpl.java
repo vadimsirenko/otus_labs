ebe56aebf2ba3b5bc305711c62568895c96fb7d2
“vasire@mail.ru” <vasire@mail.ru>
2023-08-07 21:11:47 +0300

    Основное задание

--- L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,11 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -17,6 +20,58 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
         // You code here...
+        Object confifInstanse = getConfigClassInstanse(configClass);
+
+        List<BeanDescription> beanDescriptionList = getBeanDescriptions(configClass);
+
+        reorderBuildbeanDescriptionList(beanDescriptionList);
+
+        collectCongigurationLists(confifInstanse, beanDescriptionList);
+    }
+
+    private void reorderBuildbeanDescriptionList(List<BeanDescription> beanDescriptionList) {
+        Collections.sort(beanDescriptionList);
+    }
+
+    private void collectCongigurationLists(Object confifInstanse, List<BeanDescription> beanDescriptionList) {
+        for (BeanDescription beanDescription: beanDescriptionList) {
+            Object component = null;
+            try {
+                var params = Arrays.stream(beanDescription.method.getParameterTypes()).map(pt->getAppComponent(pt)).toArray();
+                component = beanDescription.method.invoke(confifInstanse,  params);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            appComponents.add(component);
+            appComponentsByName.put(beanDescription.name, component);
+        }
+    }
+
+    private static Object getConfigClassInstanse(Class<?> configClass) {
+        Object confifInstanse;
+        try {
+            confifInstanse = configClass.getDeclaredConstructors()[0].newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+        return confifInstanse;
+    }
+
+    private List<BeanDescription> getBeanDescriptions(Class<?> configClass) {
+        List<BeanDescription> beanDescriptionList = new ArrayList<>();
+        for(Method method: configClass.getDeclaredMethods()){
+            if(method.isAnnotationPresent(AppComponent.class)){
+                var annotation = method.getAnnotation(AppComponent.class);
+                beanDescriptionList.add(new BeanDescription(annotation.order(), annotation.name(), method));
+            }
+        }
+        return beanDescriptionList;
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +82,30 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream().filter(c-> componentClass.isInstance(c)).findFirst().orElse(null);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
+
+    class BeanDescription implements Comparable<BeanDescription> {
+        int order;
+        String name;
+        Method method;
+
+        public BeanDescription(int order, String name, Method method) {
+            this.order = order;
+            this.name = name;
+            this.method = method;
+        }
+
+        @Override
+        public int compareTo(BeanDescription other) {
+            return Integer.compare(order, other.order);
+        }
+    }
+
 }
+
